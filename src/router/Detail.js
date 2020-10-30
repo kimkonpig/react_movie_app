2fbfbee03b38cd5ca5d3c8536a45a0f31e624221
kimkonpig <linggooling@gmail.com>
2020-10-30 15:31:36 +0900

    react movie app final

---
+++ src/router/Detail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const {location, history} = this.props;
+
+        if(location.state === undefined){
+            history.push('/');
+        }
+    }
+
+    render() {
+        const {location} = this.props;
+        if(location.state){
+            return (
+                <div>
+                    <span>{location.state.title}</span>
+                    <span>{location.state.year}</span>
+                </div>
+            );
+        }else{
+            return null;
+        }
+    }
+}
+
+export default Detail;
